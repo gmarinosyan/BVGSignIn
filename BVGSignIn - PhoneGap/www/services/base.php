c5bd99946441f1afbe857a89901faff27085a091
George Marinosyan <George@v1022-rn-40-97.campus-dynamic.uwaterloo.ca>
2016-01-19 22:28:04 -0500

    Removed unnecessary files

--- BVGSignIn - PhoneGap/www/services/base.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start(); 
-$dbhost = "localhost"; // this will ususally be 'localhost', but can sometimes differ
-$dbname = "bvgsignin"; // the name of the database that you are going to use for this project
-$dbuser = "admin"; // the username that you created, or were given, to access your database
-$dbpass = "bvgsignin"; // the password that you created, or were given, to access your database
-
-?>
